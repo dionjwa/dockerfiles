4700aa71606f953df082e70b73bd7b12cb3d81bd
Dion Amago <dion@transition9.com>
2017-09-20 13:02:56 -0700

    Tagging

--- update.sh
+++ update.sh
@@ -8,18 +8,18 @@ fi
 ./gen.sh > README.md
 git add .
 git commit -m "$1"
-
+git push origin
 if [ $# -eq 2 ]
-then
-	CURRENT_TAG=`git describe --abbrev=0 --tags`
-	BASE_LIST=(`echo $CURRENT_TAG | tr '.' ' '`)
-	V_MAJOR=${BASE_LIST[0]}
-	V_MINOR=${BASE_LIST[1]}
-	V_PATCH=${BASE_LIST[2]}
-	V_MINOR=$((V_MINOR + 1))
-	VERSION="$V_MAJOR.$V_MINOR.$V_PATCH"
-	echo "New version tag: $VERSION"
-	git tag "$VERSION"
+	then
+		CURRENT_TAG=`git describe --abbrev=0 --tags`
+		BASE_LIST=(`echo $CURRENT_TAG | tr '.' ' '`)
+		V_MAJOR=${BASE_LIST[0]}
+		V_MINOR=${BASE_LIST[1]}
+		V_PATCH=${BASE_LIST[2]}
+		V_MINOR=$((V_MINOR + 1))
+		VERSION="$V_MAJOR.$V_MINOR.$V_PATCH"
+		echo "New version tag: $VERSION"
+		git tag "$VERSION"
+		git push --tags origin
 fi
 
-git push --tags origin
